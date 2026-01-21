5a8287ffa1236ea22a6db79f84a379dd9572ce8a
divine02122006 <divine02122006@gmail.com>
2026-01-21 14:35:33 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
+Python
+
 from flask import Flask
 app = Flask(__name__)
 @app.route("/")
 def home():
-  return "Bonjour! cette application est déployée sur Render (PAAS)."
-  if __name__ == "__main__":
+  return "Bonjour! cette application est déployée sur Render (PaaS)."
+ f __name__ == "__main__":
     app.run()
